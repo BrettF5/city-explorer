bb83f308d719e02e753fb9c5f325876d001920b0
BrettF5 <brettfort@hotmail.com>
2023-12-13 15:32:31 -0800

    adding updates

--- city-explorer/src/App.jsx
+++ city-explorer/src/App.jsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { useState } from "react";
 const API_KEY = import.meta.env.VITE_API_KEY;
 
@@ -5,23 +6,22 @@ function App() {
 const [location, setLocation] = useState({display_name: "info about ???"});
 const [searchQuery, setSearchQuery] = useState('');
 
-console.log('API_KEY', API_KEY);
-
 async function getLocation() {
-  const API = `https://us1.locationiq.com/v1/search.php?key${API_KEY}&q=${searchQuery}&format=&format=json`
+  const API = `https://us1.locationiq.com/v1/search?key=${API_KEY}&q=${searchQuery}&format=json`
+  const response = await axios.get(API);
+  const locationObj = response.data[0];
+  setLocation(locationObj);
 }
 
 function updateQuery(event) {
   setSearchQuery(event.target.value);
 }
 
-function fetchLocation() {
-  setLocation({display_name:searchQuery});
-}
+
   return (
     <>
     <input onChange={(updateQuery)} />
-    <button onClick={fetchLocation}>Expore</button>
+    <button onClick={getLocation}>Expore</button>
     <h2>The city is: {location.display_name}</h2>
     </>
   )
